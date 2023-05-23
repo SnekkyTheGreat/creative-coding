467744bb90bf78b178c69f44862f9c058ffaaf0f
Wendy W <wendyjwhalen@gmail.com>
2023-05-23 17:22:22 -0400

    Did the assignment

--- week2/2.1.emoji/sketch.js
+++ week2/2.1.emoji/sketch.js
@@ -3,5 +3,34 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(100);
+
+  noStroke(); // set the stroke (line) color 
+  fill(204, 214, 221); // set the fill color
+
+  // draw an ellipse with 250 pixel diameter
+  // with its center at x position 200
+  // and at y position 200
+  rectMode(CENTER);
+  // skull
+  rect(200, 170, 250, 235, 155, 155, 80, 80)
+  // teeth
+  rect(200, 316, 48, 64, 0, 0, 28, 28)
+  rect(256, 300, 48, 96, 0, 0, 28, 28)
+  rect(144, 300, 48, 96, 0, 0, 28, 28)
+
+  ellipseMode(CENTER);
+  fill(41, 47, 51);
+  // eyes
+  ellipse(144, 160, 84, 84);
+  ellipse(256, 160, 84, 84);
+  // nose holes
+  ellipse(200, 240, 24, 24);
+  ellipse(190, 248, 20, 20);
+  ellipse(210, 248, 20, 20);
+  // nose framing
+  fill(204, 214, 221);
+  ellipse(177, 227, 30, 30)
+  ellipse(223, 227, 30, 30)
+
 }
--- week2/2.2.circles/sketch.js
+++ week2/2.2.circles/sketch.js
@@ -1,7 +1,45 @@
 function setup() {
-  createCanvas(1200, 1200);
+  createCanvas(1200, 900);
+  noLoop()
 }
 
 function draw() {
-  background(220);
+  // background(Math.floor(Math.random() *255), Math.floor(Math.random() *255), Math.floor(Math.random() *255));
+  background(68);
+  translate(0,0);
+  for (let x = 0; x < 12; x++){
+    for (let y = 0; y < 10; y++){
+      push();
+      translate(x * 100, y *100);
+      stroke(Math.floor(Math.random() *255), Math.floor(Math.random() *255), Math.floor(Math.random() *255) ,98);
+      strokeWeight(5);
+      fill(Math.floor(Math.random() *255), Math.floor(Math.random() *255), Math.floor(Math.random() *255), 90);
+      triangle(50,10,10,90,90,90)
+      pop();
+    }
+  }
+
+  for (let x = 0; x < 11; x++){
+    for (let y = 0; y < 8; y++){
+      push();
+      translate(x * 100, y *100);
+      stroke(Math.floor(Math.random() *255), Math.floor(Math.random() *255), Math.floor(Math.random() *255) ,98);
+      strokeWeight(5);
+      fill(Math.floor(Math.random() *255), Math.floor(Math.random() *255), Math.floor(Math.random() *255), 90);
+      ellipse(100,100,80,80);
+      pop();
+    }
+  }
+
+  for (let x = 0; x < 12; x++){
+    for (let y = 0; y < 10; y++){
+      push();
+      translate(x * 100, y *100);
+      stroke(Math.floor(Math.random() *255), Math.floor(Math.random() *255), Math.floor(Math.random() *255) ,98);
+      strokeWeight(6);
+      fill(Math.floor(Math.random() *255), Math.floor(Math.random() *255), Math.floor(Math.random() *255), 90);
+      rect(10,10,80,80);
+      pop();
+    }
+  }
 }
