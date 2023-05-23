5b34e45e84851ee47553368e26a0729870e30363
Wendy W <wendyjwhalen@gmail.com>
2023-05-23 17:49:47 -0400

    Fixed and slimmed

--- week2/2.2.circles/sketch.js
+++ week2/2.2.circles/sketch.js
@@ -11,35 +11,46 @@ function draw() {
     for (let y = 0; y < 10; y++){
       push();
       translate(x * 100, y *100);
-      stroke(Math.floor(Math.random() *255), Math.floor(Math.random() *255), Math.floor(Math.random() *255) ,98);
+      stroke(random(255), random(255), random(255),98);
       strokeWeight(5);
-      fill(Math.floor(Math.random() *255), Math.floor(Math.random() *255), Math.floor(Math.random() *255), 90);
-      triangle(50,10,10,90,90,90)
-      pop();
-    }
-  }
+      fill(random(255), random(255), random(255), 90);
+      triangle(50,25,20,75,80,75)
 
-  for (let x = 0; x < 11; x++){
-    for (let y = 0; y < 8; y++){
-      push();
-      translate(x * 100, y *100);
-      stroke(Math.floor(Math.random() *255), Math.floor(Math.random() *255), Math.floor(Math.random() *255) ,98);
-      strokeWeight(5);
-      fill(Math.floor(Math.random() *255), Math.floor(Math.random() *255), Math.floor(Math.random() *255), 90);
+
+      if (x < 11 & y < 8) {stroke(random(255), random(255), random(255), 98);
+      fill(random(255), random(255), random(255), 90);
       ellipse(100,100,80,80);
-      pop();
-    }
-  }
+      }
 
-  for (let x = 0; x < 12; x++){
-    for (let y = 0; y < 10; y++){
-      push();
-      translate(x * 100, y *100);
-      stroke(Math.floor(Math.random() *255), Math.floor(Math.random() *255), Math.floor(Math.random() *255) ,98);
-      strokeWeight(6);
-      fill(Math.floor(Math.random() *255), Math.floor(Math.random() *255), Math.floor(Math.random() *255), 90);
+      stroke(random(255), random(255), random(255), 98);
+      fill(random(255), random(255), random(255), 90);
       rect(10,10,80,80);
       pop();
-    }
-  }
+       }
+      }
+  
+
+  // for (let x = 0; x < 11; x++){
+  //   for (let y = 0; y < 8; y++){
+  //     push();
+  //     translate(x * 100, y *100);
+  //     stroke(random(255), random(255), random(255), 98);
+  //     strokeWeight(5);
+  //     fill(random(255), random(255), random(255), 90);
+  //     ellipse(100,100,80,80);
+  //     pop();
+  //   }
+  // }
+
+  // for (let x = 0; x < 12; x++){
+  //   for (let y = 0; y < 10; y++){
+  //     push();
+  //     translate(x * 100, y *100);
+  //     stroke(random(255), random(255), random(255),98);
+  //     strokeWeight(6);
+  //     fill(random(255), random(255), random(255), 90);
+  //     rect(10,10,80,80);
+  //     pop();
+  //   }
+  // } 
 }
