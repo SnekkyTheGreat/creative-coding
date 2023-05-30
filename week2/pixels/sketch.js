8eb43e9aec0d4ff0cae177bc89835b34547e063d
Wendy W <wendyjwhalen@gmail.com>
2023-05-30 17:28:28 -0400

    Forgot to save last time

--- week2/pixels/sketch.js
+++ week2/pixels/sketch.js
@@ -1,7 +1,33 @@
+let img;
+let sampleX = 36;
+let sampleY = 36;
+
+function preload(){
+  img = loadImage('https://snekkythegreat.github.io/creative-coding/week2/pixels/PixelSquiggles.jpg');
+}
+
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(500, 500);
+  img.loadPixels();
 }
 
 function draw() {
-  background(220);
+  background(0);
+
+  for (let x = 0; x <= img.width; x += sampleX){
+    for (let y = 0; y <= img.height; y += sampleY){
+      var index = (x + y * img.height) * 4;
+      var r = img.pixels[index + 0];
+      var g = img.pixels[index + 1];
+      var b = img.pixels[index + 2];
+      // main circle things
+      fill(r, g, b);
+      noStroke();
+      ellipse(x, y, sampleX, sampleY);
+      // little rect in the center that's inverted
+      rectMode(CENTER);
+      fill((255 - r), (255 - g), (255 - b));
+      rect(x, y, (sampleY - 30), (sampleX - 30));
+    }
+  }
 }
