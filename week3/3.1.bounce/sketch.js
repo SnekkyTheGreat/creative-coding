1b97603b9875809f62c5873a8b3cc451c525e9a8
Wendy W <wendyjwhalen@gmail.com>
2023-06-05 13:25:13 -0400

    Started stuff

--- week3/3.1.bounce/sketch.js
+++ week3/3.1.bounce/sketch.js
@@ -1,7 +1,47 @@
+let x = 0;
+let y = 0;
+let xspeed = 0.5;
+let yspeed = 0.5;
+let colorindx = 0;
+let colors = [
+  [0, 150, 210],
+  [80, 40, 240],
+  [190, 0, 170],
+  [240, 60, 60],
+  [200, 170, 0],
+  [90, 240, 40],
+  [10, 210, 150]
+  ]
+
+function preload(){
+  // img = loadImage('https://snekkythegreat.github.io/creative-coding/week3/3.1bounce/BluRay.png');
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 300);
+  fill(0, 150, 210);
 }
 
 function draw() {
-  background(220);
+  background(0);
+  noStroke();
+  translate(width / 2, height / 2);
+
+  ellipse(x, y, 50);
+  for (let i = 0; i < colors.length; i += 1){
+    if (x > 175 | x < -175){
+      xspeed = -xspeed;
+      fill(colors[i], colors[i + 1], colors[i + 2]);
+  }
+
+    if (y > 125 | y < -125){
+      yspeed = -yspeed;
+      fill(colors[i], colors[i + 1], colors[i + 2]);
+  }
+
+  x += xspeed;
+  y += yspeed;
+  }
+  
 }
+
